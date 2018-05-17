c081996a0a64edf40df54df03bfb10d4a74f5736
Marius Pop <marius@mlpinit.com>
2018-05-17 07:47:26 -0500

    add create movement command

---
+++ app/commands/create_movement.rb
@@ -0,0 +1,23 @@
+class CreateMovement
+  prepend SimpleCommand
+
+  attr_reader :player, :game, :from, :to, :by_dispatcher
+
+  def initialize(player:, game:, from:, to:, by_dispatcher: false)
+    @player = player
+    @game = game
+    @from = from
+    @to = to
+    @by_dispatcher = by_dispatcher
+  end
+
+  def call
+    player.movements.create!(
+      from_city_staticid: from,
+      to_city_staticid: to,
+      by_dispatcher: by_dispatcher
+    )
+    player.update!(current_location_staticid: to)
+    game.increment!(:actions_taken)
+  end
+end
---
+++ spec/commands/create_movement_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe CreateMovement do
+
+  let(:game) { Fabricate(:game) }
+  let(:user) { game.owner }
+  let(:player) { game.players.find_by(user: user) }
+
+  before(:each) do
+    CreateMovement.new(game: game, player: player, from: "1", to: "2").call
+  end
+
+  it "sets from_city_staticid" do
+    expect(Movement.last.from_city_staticid).to eq('1')
+  end
+
+  it "sets to_city_staticid" do
+    expect(Movement.last.to_city_staticid).to eq('2')
+  end
+
+  it "sets player_id" do
+    expect(Movement.last.player_id).to eq(player.id)
+  end
+
+  it "sets by_dispatcher" do
+    expect(Movement.last.by_dispatcher).to be(false)
+  end
+
+end
