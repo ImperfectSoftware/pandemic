04ec64acf5b1d6ef295198ea0fbe7d9e5fe8ca57
smurfik <tammy@mlpinit.com>
2018-05-19 10:00:41 -0500

    alphabetize translations and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :games, only: [:create, :update], format: :json do
-    resources :invitations, only: [:create, :update, :destroy], format: :json
-    resources :shuttle_flights, only: :create, format: :json
-    resources :direct_flights, only: :create, format: :json
     resources :charter_flights, only: :create, format: :json
-    resources :line_movements, only: :create, format: :json
+    resources :direct_flights, only: :create, format: :json
     resources :get_cards, only: :create, format: :json
     resources :give_cards, only: :create, format: :json
+    resources :invitations, only: [:create, :update, :destroy], format: :json
+    resources :line_movements, only: :create, format: :json
+    resources :shuttle_flights, only: :create, format: :json
   end
   post 'authenticate', to: 'authentication#authenticate'
 end
