b02aa6a5cc644e4cc588e81d08b50f9daf7840fe
Marius Pop <marius@mlpinit.com>
2018-05-19 11:41:15 -0500

    refactor get cards request spec

--- spec/requests/get_cards_request_spec.rb
+++ spec/requests/get_cards_request_spec.rb
@@ -16,55 +16,49 @@ RSpec.describe GetCardsController, type: :request do
 
   it "returns an error if the other player is not at the same location" do
     player.update!(current_location_staticid: WorldGraph.cities[25].staticid)
-    post "/games/#{game.id}/get_cards", params: {
-      player_id: player.id
-    }.to_json, headers: headers
+    trigger_post
     expect(error).to eq(I18n.t("share_cards.not_the_same_location"))
   end
 
   it "returns an error if the other player is not in possession of the card" do
     player.update!(cards_composite_ids: [])
-    post "/games/#{game.id}/get_cards", params: {
-      player_id: player.id
-    }.to_json, headers: headers
+    trigger_post
     expect(error).to eq(I18n.t("share_cards.not_an_owner"))
   end
 
   context "with valid request" do
     it "stores the current player's id in the to_player_id field" do
-      post "/games/#{game.id}/get_cards", params: {
-        player_id: player.id
-      }.to_json, headers: headers
+      trigger_post
       expect(ShareCard.last.to_player_id).to eq(current_player.id)
     end
 
     it "stores the other player's id in the from_player_id field" do
-      post "/games/#{game.id}/get_cards", params: {
-        player_id: player.id
-      }.to_json, headers: headers
+      trigger_post
       expect(ShareCard.last.from_player_id).to eq(player.id)
     end
 
     it "sets accepted to false" do
-      post "/games/#{game.id}/get_cards", params: {
-        player_id: player.id
-      }.to_json, headers: headers
+      trigger_post
       expect(ShareCard.last.accepted).to be(false)
     end
 
     it "stores the card composite id" do
-      post "/games/#{game.id}/get_cards", params: {
-        player_id: player.id
-      }.to_json, headers: headers
+      trigger_post
       expect(ShareCard.last.city_staticid)
         .to eq(player.current_location.staticid)
     end
 
     it "stores the card creator id" do
-      post "/games/#{game.id}/get_cards", params: {
-        player_id: player.id
-      }.to_json, headers: headers
+      trigger_post
       expect(ShareCard.last.creator_id).to eq(current_player.id)
     end
+  end
+
+  private
+
+  def trigger_post
+    post "/games/#{game.id}/get_cards", params: {
+      player_id: player.id
+    }.to_json, headers: headers
   end
 end
