7106934db9d41bcdcf042786dcaad5bb9a140de1
Marius Pop <marius@mlpinit.com>
2018-05-19 11:35:57 -0500

    refactor direct flights spec

--- spec/requests/direct_flights_request_spec.rb
+++ spec/requests/direct_flights_request_spec.rb
@@ -9,72 +9,66 @@ RSpec.describe DirectFlightsController, type: :request do
   let(:current_player) { current_user.players.find_by(game: game) }
   let(:player) { Fabricate(:player, game: game) }
   let(:user) { user.players.find_by(game: game) }
+  let(:city) { WorldGraph.cities[10] }
+  let(:composite_id) { city.composite_id }
 
   before(:each) do
     game.update(player_turn_ids: [current_player.id, player.id])
   end
 
   it "returns an error if no player card passed in" do
-    post "/games/#{game.id}/direct_flights", params: {}, headers: headers
+    trigger_post
     expect(error).to eq(I18n.t("player_actions.city_card_composite_id"))
   end
 
   it "returns an error if a special player card is passed in" do
-    post "/games/#{game.id}/direct_flights", params: {
-      player_card_composite_id: SpecialCard.events.first.composite_id
-    }.to_json, headers: headers
+    trigger_post(id: SpecialCard.events.first.composite_id)
     expect(error).to eq(I18n.t("player_actions.city_card_composite_id"))
   end
 
   context "with valid request" do
-    let(:city) { WorldGraph.cities[10] }
-    let(:composite_id) { city.composite_id }
     before(:each) do
       current_player.update!(cards_composite_ids: [composite_id])
     end
 
     it "creates a movement with the to location set to the card passed in" do
-      post "/games/#{game.id}/direct_flights", params: {
-        player_card_composite_id: composite_id
-      }.to_json, headers: headers
+      trigger_post
       expect(Movement.last.to_city_staticid).to eq(city.staticid)
     end
 
     it "sets movement's from location to the player's current location" do
-      post "/games/#{game.id}/direct_flights", params: {
-        player_card_composite_id: composite_id
-      }.to_json, headers: headers
+      trigger_post
       expect(Movement.last.from_city_staticid)
         .to eq(current_player.current_location_staticid)
     end
 
     it "sets by_dispatcher to false" do
-      post "/games/#{game.id}/direct_flights", params: {
-        player_card_composite_id: composite_id
-      }.to_json, headers: headers
+      trigger_post
       expect(Movement.last.by_dispatcher).to be(false)
     end
 
     it "sets the current player's location to the new location" do
-      post "/games/#{game.id}/direct_flights", params: {
-        player_card_composite_id: composite_id
-      }.to_json, headers: headers
+      trigger_post
       expect(current_player.reload.current_location).to eq(city)
     end
 
     it "increments actions taken" do
-      post "/games/#{game.id}/direct_flights", params: {
-        player_card_composite_id: composite_id
-      }.to_json, headers: headers
+      trigger_post
       expect(game.reload.actions_taken).to eq(1)
     end
 
     it "removes used player card from player's inventory" do
-      post "/games/#{game.id}/direct_flights", params: {
-        player_card_composite_id: composite_id
-      }.to_json, headers: headers
+      trigger_post
       expect(current_player.reload.cards_composite_ids.include?(composite_id))
         .to be(false)
     end
+  end
+
+  private
+
+  def trigger_post(id: composite_id)
+    post "/games/#{game.id}/direct_flights", params: {
+      player_card_composite_id: id
+    }.to_json, headers: headers
   end
 end
