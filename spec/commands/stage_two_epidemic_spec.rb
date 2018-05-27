52852498cb0a64c08ed6791950d73c36ccdb797c
Marius Pop <marius@mlpinit.com>
2018-05-26 21:47:32 -0500

    fix stage two epidemic test medi/quarantine specialist

--- spec/commands/stage_two_epidemic_spec.rb
+++ spec/commands/stage_two_epidemic_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe StageTwoEpidemic do
   let(:los_angeles) { WorldGraph.cities[1] }
   let(:san_francisco) { WorldGraph.cities[0] }
+  let(:current_player) { game.players.first }
   let(:game) do
     Fabricate(
       :game,
@@ -14,30 +15,30 @@ RSpec.describe StageTwoEpidemic do
 
   let(:command) { StageTwoEpidemic.new(game: game) }
 
-  it "updates nr of intensified cards to 0" do
+  before(:each) do
+    current_player.update!(role: Player.roles.keys[0])
     command.call
+  end
+
+  it "updates nr of intensified cards to 0" do
     expect(game.reload.nr_of_intensified_cards).to eq(0)
   end
 
   it "infects San Francisco" do
-    command.call
     infection = game.infections.find_by(city_staticid: san_francisco.staticid)
     expect(infection.quantity).to eq(1)
   end
 
   it "infects Los Angeles" do
-    command.call
     infection = game.infections.find_by(city_staticid: los_angeles.staticid)
     expect(infection.quantity).to eq(1)
   end
 
   it "uses two infection cards" do
-    command.call
     expect(game.reload.used_infection_card_city_staticids.count).to eq(2)
   end
 
   it "removes used cards from unused pile" do
-    command.call
     expect(game.reload.unused_infection_card_city_staticids.count).to eq(0)
   end
 end
