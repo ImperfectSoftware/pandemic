ae290d88d29bc3aef7a429d1d43a8769c37aaf90
Marius Pop <marius@mlpinit.com>
2018-05-27 14:08:31 -0500

    move turn validation back to controller

--- app/controllers/games/stage_two_epidemics_controller.rb
+++ app/controllers/games/stage_two_epidemics_controller.rb
@@ -1,9 +1,21 @@
 class Games::StageTwoEpidemicsController < ApplicationController
+  before_action :check_for_potential_create_errors, only: :create
+
   def create
-    command = StageTwoEpidemic.new(game: game, player: current_player)
-    command.call
-    if command.errors.any?
-      render json: { error: command.errors[:allowed].first }
-    end
+    StageTwoEpidemic.new(game: game).call
+  end
+
+  private
+
+  def create_error_message
+    @create_error_message ||=
+      begin
+        case
+        when current_player != active_player
+          I18n.t('player_actions.bad_turn')
+        when !game.between_epidemic_stages?
+          I18n.t("errors.not_authorized")
+        end
+      end
   end
 end
--- spec/commands/stage_two_epidemic_spec.rb
+++ spec/commands/stage_two_epidemic_spec.rb
@@ -3,65 +3,42 @@ require 'rails_helper'
 RSpec.describe StageTwoEpidemic do
   let(:los_angeles) { WorldGraph.cities[1] }
   let(:san_francisco) { WorldGraph.cities[0] }
+  let(:game) do
+    Fabricate(
+      :game,
+      nr_of_intensified_cards: 4,
+      unused_infection_card_city_staticids: %w{0 1},
+      discarded_special_player_card_ids: %w{0 0}
+    )
+  end
+  let(:current_player) { game.players.first }
+  let(:player) { Fabricate(:player, game: game) }
+  let(:command) { StageTwoEpidemic.new(game: game) }
 
-  context "valid call" do
-    let(:game) do
-      Fabricate(
-        :game,
-        nr_of_intensified_cards: 4,
-        unused_infection_card_city_staticids: %w{0 1},
-        discarded_special_player_card_ids: %w{0 0}
-      )
-    end
-    let(:current_player) { game.players.first }
-    let(:player) { Fabricate(:player, game: game) }
-    let(:command) { StageTwoEpidemic.new(game: game, player: current_player) }
-
-    before(:each) do
-      current_player.update!(role: Player.roles.keys[0])
-      command.call
-    end
-
-    it "updates nr of intensified cards to 0" do
-      expect(game.reload.nr_of_intensified_cards).to eq(0)
-    end
-
-    it "infects San Francisco" do
-      infection = game.infections.find_by(city_staticid: san_francisco.staticid)
-      expect(infection.quantity).to eq(1)
-    end
-
-    it "infects Los Angeles" do
-      infection = game.infections.find_by(city_staticid: los_angeles.staticid)
-      expect(infection.quantity).to eq(1)
-    end
-
-    it "uses two infection cards" do
-      expect(game.reload.used_infection_card_city_staticids.count).to eq(2)
-    end
-
-    it "removes used cards from unused pile" do
-      expect(game.reload.unused_infection_card_city_staticids.count).to eq(0)
-    end
+  before(:each) do
+    current_player.update!(role: Player.roles.keys[0])
+    command.call
   end
 
-  context "validation" do
-    let(:game) { Fabricate(:game) }
-    let(:current_player) { game.players.first }
-    let(:player) { Fabricate(:player, game: game) }
-    let(:command) { StageTwoEpidemic.new(game: game, player: current_player) }
+  it "updates nr of intensified cards to 0" do
+    expect(game.reload.nr_of_intensified_cards).to eq(0)
+  end
 
-    it "does not perform stage two if not invoked by active player" do
-      game.update!(turn_nr: 2, player_turn_ids: [current_player.id, player.id])
-      command.call
-      expect(command.errors[:allowed].first)
-        .to eq(I18n.t('player_actions.bad_turn'))
-    end
+  it "infects San Francisco" do
+    infection = game.infections.find_by(city_staticid: san_francisco.staticid)
+    expect(infection.quantity).to eq(1)
+  end
 
-    it "does not perform stage tow if not in between epidemic stages" do
-      command.call
-      expect(command.errors[:allowed].first)
-        .to eq(I18n.t("errors.not_authorized"))
-    end
+  it "infects Los Angeles" do
+    infection = game.infections.find_by(city_staticid: los_angeles.staticid)
+    expect(infection.quantity).to eq(1)
+  end
+
+  it "uses two infection cards" do
+    expect(game.reload.used_infection_card_city_staticids.count).to eq(2)
+  end
+
+  it "removes used cards from unused pile" do
+    expect(game.reload.unused_infection_card_city_staticids.count).to eq(0)
   end
 end
