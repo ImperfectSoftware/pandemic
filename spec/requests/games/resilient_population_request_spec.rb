1b2027661fb8a01679aacd4db95ef825f21c5b03
Marius Pop <marius@mlpinit.com>
2018-05-27 13:12:20 -0500

    make sure test covers the proper condition

--- spec/requests/games/resilient_population_request_spec.rb
+++ spec/requests/games/resilient_population_request_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Games::ResilientPopulationsController, type: :request do
 
   let(:game) { Fabricate(:game) }
   let(:current_user) { game.owner }
+  let(:resilient_population) { SpecialCard.resilient_population }
 
   context "GET request" do
     it "returns error message if not between epidemic stages" do
@@ -42,35 +43,36 @@ RSpec.describe Games::ResilientPopulationsController, type: :request do
         expect(error).to eq(I18n.t("player_actions.city_staticid"))
       end
 
-      it "returns error message if city is not part of the used cards" do
-        trigger_post(city_staticid: '35')
-        expect(error).to eq(I18n.t("errors.not_authorized"))
-      end
-
       it "returns error if current_player does not own the resilient card" do
         trigger_post(city_staticid: '7')
         expect(error).to eq(I18n.t("errors.not_authorized"))
       end
 
       context "when player is in possession of the resilient popylation card" do
-        let(:resilient_population) { SpecialCard.resilient_population }
         let(:player) { game.players.first }
         before(:each) do
           player
             .update!(cards_composite_ids: [resilient_population.composite_id])
-          trigger_post(city_staticid: '7')
+        end
+
+        it "returns error message if city is not part of the used cards" do
+          trigger_post(city_staticid: '35')
+          expect(error).to eq(I18n.t("errors.not_authorized"))
         end
 
         it "removes infectin card from infection card pile" do
+          trigger_post(city_staticid: '7')
           expect(game.reload.unused_infection_card_city_staticids)
             .to eq(%w{7 8})
         end
 
         it "removes special card from player's inventory" do
+          trigger_post(city_staticid: '7')
           expect(player.reload.owns_card?(resilient_population)).to be(false)
         end
 
         it "places card in discarded special cards" do
+          trigger_post(city_staticid: '7')
           discarded_cards = game.reload.discarded_special_player_card_ids
           expect(discarded_cards.include?(resilient_population.staticid))
             .to be(true)
