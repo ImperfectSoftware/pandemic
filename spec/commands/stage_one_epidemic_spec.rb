43ea8fd913f9e0831526f29d00d8391c5d27474d
Marius Pop <marius@mlpinit.com>
2018-05-26 21:50:00 -0500

    fix stage one epidemic test medi/quarantine specialist

--- spec/commands/stage_one_epidemic_spec.rb
+++ spec/commands/stage_one_epidemic_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe StageOneEpidemic do
   let(:game) { Fabricate(:game) }
+  let(:current_player) { game.players.first }
   let(:command) { StageOneEpidemic.new(game: game) }
 
   context "general case" do
@@ -10,25 +11,23 @@ RSpec.describe StageOneEpidemic do
         used_infection_card_city_staticids: %w{5 6 7 8},
         unused_infection_card_city_staticids: %w{0 1 2 3 4}
       )
+      current_player.update!(role: Player.roles.keys[0])
+      command.call
     end
 
     it "places 3 infection cards at the cards location" do
-      command.call
       expect(game.infections.find_by(city_staticid: '0').quantity).to eq(3)
     end
 
     it "removes cards from used infection cards pile" do
-      command.call
       expect(game.reload.used_infection_card_city_staticids.count).to eq(0)
     end
 
     it "places cards back into unused pile" do
-      command.call
       expect(game.reload.unused_infection_card_city_staticids.count).to eq(9)
     end
 
     it "updates nr of intensified cards" do
-      command.call
       expect(game.reload.nr_of_intensified_cards).to eq(5)
     end
   end
