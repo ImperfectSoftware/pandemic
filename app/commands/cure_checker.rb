8083adb2137b053db7a28f13e7cfbadb1086161b
Marius Pop <marius@mlpinit.com>
2018-06-22 07:04:12 -0500

    add cure checker command

---
+++ app/commands/cure_checker.rb
@@ -0,0 +1,48 @@
+class CureChecker
+  prepend SimpleCommand
+
+  attr_reader :game, :player
+
+  def initialize(game:, player:)
+    @game = game
+    @player = player
+  end
+
+  def call
+    return no_response unless player.at_research_station?
+    return no_response unless enough_cards_to_discover_cure
+    return no_response unless player.has_actions_left?
+    return yes_response
+  end
+
+  private
+
+  def most_city_cards
+    @most_city_cards ||=
+      begin
+        pair = player.cities.group_by(&:color).map do |color, cities|
+          [color, cities.count]
+        end.sort do |(_, count_one), (_, count_two)|
+          count_two <=> count_one
+        end.first
+        OpenStruct.new(color: pair.first, count: pair.last)
+      end
+  end
+
+  def enough_cards_to_discover_cure
+    most_city_cards.count >=
+      if player.scientist?
+        4
+      else
+        5
+      end
+  end
+
+  def no_response
+    OpenStruct.new(can_discover_cure: false, color: 'none')
+  end
+
+  def yes_response
+    OpenStruct.new(can_discover_cure: true, color: most_city_cards.color)
+  end
+end
---
+++ spec/commands/cure_checker_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe CureChecker do
+
+
+  let(:game) { Fabricate(:game) }
+  let(:player_one) { game.players.find_by(user: game.owner) }
+  let(:player_two) { Fabricate(:player, game: game) }
+  let(:blue_cities) { WorldGraph.cities[18,6] }
+
+  it "can't cure disease if the player is not at a research station" do
+    command = CureChecker.call(game: game, player: player_one)
+    expect(command.result.can_discover_cure).to eq(false)
+  end
+
+  it "can't cure disease if the player doesn't have enough city cards" do
+    game.research_stations.create!(city_staticid: player_one.location.staticid)
+    expect(player_one.at_research_station?).to eq(true)
+    command = CureChecker.call(game: game, player: player_one)
+    expect(command.result.can_discover_cure).to eq(false)
+  end
+
+  it "can't cure disease if it's not the player's turn" do
+    five_blue_cities = blue_cities.map(&:composite_id)
+    game.update(player_turn_ids: [player_two.id, player_one.id])
+    game.research_stations.create!(city_staticid: player_one.location.staticid)
+    expect(player_one.at_research_station?).to eq(true)
+    player_one.update!(cards_composite_ids: five_blue_cities)
+    command = CureChecker.call(game: game, player: player_one)
+    expect(command.result.can_discover_cure).to eq(false)
+  end
+
+  context "when the disease is curable" do
+    before(:each) do
+      game.update(player_turn_ids: [player_one.id, player_two.id])
+      game.research_stations
+        .create!(city_staticid: player_one.location.staticid)
+    end
+
+    it "requires 4 cards to cure disease for a scientist" do
+      four_blue_cities = blue_cities[0,4].map(&:composite_id)
+      player_one.update!(
+        cards_composite_ids: four_blue_cities,
+        role: 'scientist'
+      )
+      command = CureChecker.call(game: game, player: player_one)
+      expect(command.result.can_discover_cure).to eq(true)
+      expect(command.result.color).to eq(blue_cities.first.color)
+    end
+
+    it "requires 5 cards to cure disease for non scientist" do
+      five_blue_cities = blue_cities.map(&:composite_id)
+      player_one.update!(cards_composite_ids: five_blue_cities, role: 'medic')
+      command = CureChecker.call(game: game, player: player_one)
+      expect(command.result.can_discover_cure).to eq(true)
+      expect(command.result.color).to eq(blue_cities.first.color)
+    end
+
+    it "can cure disease if player owns more than 5 cards" do
+      player_one.update!(
+        cards_composite_ids: blue_cities.map(&:composite_id),
+        role: 'medic'
+      )
+      command = CureChecker.call(game: game, player: player_one)
+      expect(command.result.can_discover_cure).to eq(true)
+      expect(command.result.color).to eq(blue_cities.first.color)
+    end
+  end
+end
