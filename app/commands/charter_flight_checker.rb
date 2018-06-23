64ecc9a63384837c46fd727e852b7e4e6c97154a
Marius Pop <marius@mlpinit.com>
2018-06-23 09:43:44 -0500

    don't display charter flight option if player is in the same city

--- app/commands/charter_flight_checker.rb
+++ app/commands/charter_flight_checker.rb
@@ -1,13 +1,19 @@
 class CharterFlightChecker
   prepend SimpleCommand
 
-  attr_reader :player
-  def initialize(player:)
+  attr_reader :player, :city_staticid
+  def initialize(player:, city_staticid:)
     @player = player
+    @city_staticid = city_staticid
   end
 
   def call
     return false unless player.has_actions_left?
+    return false if location == player.location
     player.owns_card?(player.location)
+  end
+
+  def location
+    City.find(city_staticid)
   end
 end
--- spec/commands/charter_flight_checker_spec.rb
+++ spec/commands/charter_flight_checker_spec.rb
@@ -4,21 +4,35 @@ RSpec.describe CharterFlightChecker do
   let(:game) { Fabricate(:two_player_game) }
 
   it "returns false if player has no moves left" do
-    command = CharterFlightChecker.call(player: game.players.last)
+    command = CharterFlightChecker.call(
+      player: game.players.last,
+      city_staticid: '1'
+    )
     expect(command.result).to eq(false)
   end
 
   it "returns false if the player doesn't own the card" do
     player = game.players.first
     player.update!(cards_composite_ids: [])
-    command = CharterFlightChecker.call(player: player)
+    command = CharterFlightChecker.call(player: player, city_staticid: '1')
+    expect(command.result).to eq(false)
+  end
+
+  it "returns false if the location is the player's location" do
+    player = game.players.first
+    player.update!(cards_composite_ids: [player.location.composite_id])
+    command = CharterFlightChecker.call(
+      player: player,
+      city_staticid: player.location.staticid
+    )
     expect(command.result).to eq(false)
   end
 
   it "returns true if player owns the card" do
     player = game.players.first
     player.update!(cards_composite_ids: [player.location.composite_id])
-    command = CharterFlightChecker.call(player: player)
+    staticid = ((player.location.staticid.to_i + 10) % 47).to_s
+    command = CharterFlightChecker.call(player: player, city_staticid: staticid)
     expect(command.result).to eq(true)
   end
 end
