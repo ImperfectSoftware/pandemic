22ea633d8f691fe89bdc22e07e63003266c040b9
Marius Pop <marius@mlpinit.com>
2018-06-23 08:46:05 -0500

    revemo the use of dahsed_name

--- spec/poros/city_spec.rb
+++ spec/poros/city_spec.rb
@@ -5,14 +5,4 @@ RSpec.describe City do
     city = WorldGraph.cities.sample
     expect(city).to be(City.find(city.staticid))
   end
-
-  it 'formats multi part cities correctly' do
-    city = WorldGraph.cities[8]
-    expect(city.dashed_name).to eq('new-york')
-  end
-
-  it 'formats st petersburg correctly' do
-    city = WorldGraph.cities[23]
-    expect(city.dashed_name).to eq('st-petersburg')
-  end
 end
