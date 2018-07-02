3616bc4a65b3f78ed135b17ed80fb8ae7675eecf
Marius Pop <marius@mlpinit.com>
2018-07-02 06:53:58 -0500

    broadcast game update on discarding player card

--- app/controllers/games/discard_city_cards_controller.rb
+++ app/controllers/games/discard_city_cards_controller.rb
@@ -2,6 +2,7 @@ class Games::DiscardCityCardsController < ApplicationController
 
   def destroy
     current_player.update!(cards_composite_ids: remaining_cards)
+    send_game_broadcast
   end
 
   private
@@ -12,5 +13,17 @@ class Games::DiscardCityCardsController < ApplicationController
 
   def remaining_cards
     current_player.cards_composite_ids - [city_card&.composite_id]
+  end
+
+  def send_game_broadcast
+    payload = JSON.parse(ApplicationController.new.render_to_string(
+      'games/show',
+      locals: { game: StartedGameDecorator.new(game) }
+    ))
+    ActionCable.server.broadcast(
+      "game_channel:#{game.id}",
+      game_update: true,
+      game: payload
+    )
   end
 end
