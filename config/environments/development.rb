038a6649190ed03a2d789cdadea6f566f29f1db3
Marius Pop <marius@mlpinit.com>
2018-07-08 09:33:15 -0500

    run action cable on separate server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.web_socket_server_url = 'ws://localhost:28080'
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
