1d8a716127b8c5b807a3fb987ef1398e231e9446
Marius Pop <marius@mlpinit.com>
2018-07-08 18:48:32 -0500

    add create movement from proposal interactor

---
+++ app/commands/create_movement_from_proposal.rb
@@ -0,0 +1,95 @@
+class CreateMovementFromProposal
+  include GameBroadcast
+
+  prepend SimpleCommand
+
+  private
+
+  delegate :game, :airlift?, :creator, :player, to: :proposal
+
+  attr_reader :proposal, :requester
+  def initialize(proposal, requester)
+    @proposal = proposal
+    @requester = requester
+  end
+
+  def call
+    validate_command
+    return if errors.any?
+    proposal.update!(accepted: true)
+    CreateMovement
+      .call(game: game, player: player, from: from, to: to, airlift: airlift?)
+    if airlift?
+      place_airlift_card_in_game_discarded_special_player_cards!
+      discard_airlift_card_from_movement_proposal_creator!
+    end
+    send_game_broadcast
+  end
+
+  def from
+    player.location_staticid
+  end
+
+  def to
+    proposal.city_staticid
+  end
+
+  def place_airlift_card_in_game_discarded_special_player_cards!
+    game.discarded_special_player_card_ids << airlift_card.staticid
+    game.save!
+  end
+
+  def discard_airlift_card_from_movement_proposal_creator!
+    creator.cards_composite_ids.delete(airlift_card.composite_id)
+    creator.save!
+  end
+
+  def airlift_card
+    SpecialCard.events.find(&:airlift?)
+  end
+
+  # Private: Validate that creating a movement is allowed.
+  #
+  # A movement proposal originating from an airlift event only needs to
+  # validated that the correct player is accepting the proposal.
+  #
+  # When the movement proposal does not originate from an airlift event card we
+  # also need to validate the following:
+  #
+  # Validate that there are enough actions left to be able to apply the
+  # proposal.
+  #
+  # Ensure that the proposal was created during the same round/turn the player
+  # is accepting the proposal.
+  #
+  # The dispatcher can act as another player and create a movement proposal to a
+  # neighboring location. The dispatcher can also move the player to a location
+  # with another existing player. We need to validated that the location the
+  # proposal was created for is stil valid. A location from a movement proposal
+  # could be invalid if the player moved to another location since the movement
+  # proposal was created.
+  def validate_command
+    if requester != player
+      errors.add(:allowed, I18n.t("errors.not_authorized"))
+    end
+    unless airlift?
+      if game.actions_taken == 4
+        errors.add(:allowed, I18n.t("player_actions.no_actions_left"))
+      end
+      if proposal.turn_nr != game.turn_nr
+        errors.add(:allowed, I18n.t("movement_proposals.expired"))
+      end
+      if destination_is_not_a_neighbor? && another_player_is_not_at_destination?
+        errors.add(:allowed, I18n.t("movement_proposals.not_allowed"))
+      end
+    end
+  end
+
+  def destination_is_not_a_neighbor?
+    !player.location.neighbors_staticids.include?(to)
+  end
+
+  def another_player_is_not_at_destination?
+    !game.players.map(&:location).map(&:staticid).include?(to)
+  end
+end
--- app/controllers/movement_proposals_controller.rb
+++ app/controllers/movement_proposals_controller.rb
@@ -1,6 +1,4 @@
 class MovementProposalsController < PlayerActionsController
-  before_action :check_for_potential_update_errors, only: :update
-
   def create
     proposal = current_player.created_movement_proposals.create!(
       player_id: params[:player_id],
@@ -15,28 +13,15 @@ class MovementProposalsController < PlayerActionsController
 
   def update
     if params[:accepted]
-      movement_proposal.update!(accepted: true)
-      CreateMovement.new(
-        game: game,
-        player: puppet_player,
-        from: puppet_player.location_staticid,
-        to: city_staticid,
-        airlift: movement_proposal.airlift?
-      ).call
-      if movement_proposal.airlift?
-        game.discarded_special_player_card_ids << airlift_card.staticid
-        game.save!
-        movement_proposal.creator.update!(cards_composite_ids: remaining_cards)
+      command = CreateMovementFromProposal
+        .call(movement_proposal, current_player)
+      if command.errors.any?
+        render json: { error: command.errors[:allowed].first }
       end
-      send_game_broadcast
     end
   end
 
   private
-
-  def check_for_potential_update_errors
-    render json: { error: update_error_message } if update_error_message
-  end
 
   def create_error_message
     @create_error_message ||=
@@ -44,31 +29,10 @@ class MovementProposalsController < PlayerActionsController
         case
         when params[:city_staticid].blank? || params[:player_id].blank?
           I18n.t("errors.missing_param")
-        when using_airlift?
-          # we need to allow this action if city is specified
         when !destination_is_a_neighbor? && !other_player_at_destination?
           I18n.t("movement_proposals.not_allowed")
         when !current_player.dispatcher?
           I18n.t("dispatcher.must_be_a_dispatcher")
-        end
-      end
-  end
-
-  def update_error_message
-    @update_error_message ||=
-      begin
-        case
-        when !params[:accepted] || movement_proposal.airlift?
-          # we don't need to check for errors if not accpeted
-          # any city is allowed with airlift
-        when puppet_player != current_player
-          I18n.t("errors.not_authorized")
-        when game.actions_taken == 4
-          I18n.t("player_actions.no_actions_left")
-        when !destination_is_a_neighbor? && !other_player_at_destination?
-          I18n.t("movement_proposals.not_allowed")
-        when movement_proposal.turn_nr != game.turn_nr
-          I18n.t("movement_proposals.expired")
         end
       end
   end
@@ -92,7 +56,7 @@ class MovementProposalsController < PlayerActionsController
   end
 
   def city_staticid
-    params[:city_staticid] || movement_proposal.city_staticid
+    params[:city_staticid]
   end
 
   def location
@@ -103,16 +67,19 @@ class MovementProposalsController < PlayerActionsController
     params[:player_id] || movement_proposal.player_id
   end
 
-  def using_airlift?
-    !!current_player.events.find(&:airlift?) && params[:airlift]
-  end
-
   def airlift_card
     SpecialCard.events.find(&:airlift?)
   end
 
-  def remaining_cards
-    puppet_player.cards_composite_ids - [airlift_card.composite_id]
+  def place_airlift_card_in_game_discarded_special_player_cards
+    game.discarded_special_player_card_ids << airlift_card.staticid
+    game.save!
+  end
+
+  def discard_airlift_card_from_movement_proposal_creator
+    movement_proposal.creator.cards_composite_ids
+      .delete(airlift_card.composite_id)
+    movement_proposal.creator.save!
   end
 
   def send_movement_proposal_broadcast(proposal)
